10a116b2e18f57388ce985545531da4bc0383d76
Vinsi Leo <Vthulaseedharan@mafcarrefour.com>
2021-08-26 09:00:18 +0400

    Initial Commit

---
+++ BitExchange/BitExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitExchangeApp.swift
+//  BitExchange
+//
+//  Created by Vinsi on 26/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct BitExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BitExchange/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BitExchange
+//
+//  Created by Vinsi on 26/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
